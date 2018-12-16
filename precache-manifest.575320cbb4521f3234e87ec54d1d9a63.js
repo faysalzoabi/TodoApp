982c56c40227755b8abeb75e45692765c5299a16
Faysal AlZoubi <faysal.zoabi@gmail.com>
2018-12-16 01:25:53 +0100

    Updates

---
+++ precache-manifest.575320cbb4521f3234e87ec54d1d9a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d779971c9701e8dcfd5b",
+    "url": "/TodoApp/static/js/runtime~main.d779971c.js"
+  },
+  {
+    "revision": "c845c2d662a7c5af6cd3",
+    "url": "/TodoApp/static/js/main.c845c2d6.chunk.js"
+  },
+  {
+    "revision": "aac75bbadb65a070ad02",
+    "url": "/TodoApp/static/js/1.aac75bba.chunk.js"
+  },
+  {
+    "revision": "c845c2d662a7c5af6cd3",
+    "url": "/TodoApp/static/css/main.65430fdc.chunk.css"
+  },
+  {
+    "revision": "a78819bb264e5b9ff23c4f8ad1552da6",
+    "url": "/TodoApp/index.html"
+  }
+];
